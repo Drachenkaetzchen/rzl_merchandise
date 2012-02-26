96f93b73b3aabbc633e705c1be615e6cd3fd31c5
Severin Schols <severin@schols.de>
2012-02-26 22:12:41 +0100

    Added pictures

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $items = array();
 $item = new Item();
 $item->setName("RaumZeitLabor T-Shirt");
 $item->setDescription("Das Super RZL Shirt");
-$item->setImage("shirt.gif");
+$item->setImage("tshirt.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
 $item->addSizePrice(new SizePrice("L", 13.90));
@@ -19,7 +19,7 @@ $item->setName("RZL LabCoat");
 $item->setDescription(	"Das Pflichtprogramm für jeden Laboraten! Ein bestickter Labcoat, der auch " .
 						"wunderbar für Ätzsessions geeignet ist. Die Bestickung wird von Unicorn oder Inte ".
 						"mittels unserer Stickmaschine vorgenommen." );
-$item->setImage("shirt.gif");
+$item->setImage("labcoat.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
 $item->addSizePrice(new SizePrice("L", 13.90));
