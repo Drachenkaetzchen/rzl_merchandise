7c7bca9303d7f6e2c85121a45145b864d79cff86
Simon Elsbrock <simon@iodev.org>
2012-02-27 17:11:51 +0100

    improve quality of thumbnails and rename "schuertze" to "schuerze"

--- config.php
+++ config.php
@@ -54,7 +54,7 @@ $items[] = $item;
 $item = new Item();
 $item->setName("Schürtze");
 $item->setDescription(	"Hoodie Hoodie Hoodie.");
-$item->setImage("schuertze.jpg");
+$item->setImage("schuerze.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
 $item->addSizePrice(new SizePrice("L", 13.90));
