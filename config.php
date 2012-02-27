a6cbbc0895227e2dbb18a8ed3d2c93430a6d277a
Simon Elsbrock <simon@iodev.org>
2012-02-27 18:46:52 +0100

    modify descriptions, they're still far from perfect though ;)

--- config.php
+++ config.php
@@ -16,8 +16,21 @@ $item->addSizePrice(new SizePrice("S", 11.90));
 $items[] = $item;
 
 $item = new Item();
+$item->setName("T-Shirt");
+$item->setDescription(	"Das offizielle RaumZeitLabor T-Shirt, passend zu jeder Gelegenheit. " .
+                        "Der Verkaufsschlager jetzt zum Sonderpreis!");
+$item->setImage("tshirt.jpg");
+$item->addSizePrice(new SizePrice("XXL", 15.90));
+$item->addSizePrice(new SizePrice("XL", 14.90));
+$item->addSizePrice(new SizePrice("L", 13.90));
+$item->addSizePrice(new SizePrice("M", 12.90));
+$item->addSizePrice(new SizePrice("S", 11.90));
+
+$items[] = $item;
+
+$item = new Item();
 $item->setName("Hemd");
-$item->setDescription(	"Auch Nerds müssen mal raus - und wenn's schick aussehen soll, gibt es dafür das Hemd.");
+$item->setDescription(	"Auch Nerds müssen mal raus - und wenns schick aussehen soll, gibt es dafür das Hemd.");
 $item->setImage("hemd.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
@@ -29,7 +42,7 @@ $items[] = $item;
 
 $item = new Item();
 $item->setName("Hoodie");
-$item->setDescription(	"Hoodie Hoodie Hoodie.");
+$item->setDescription(	"Wer mag es nicht, das flauschige Allwetterkleidungsstück für den Durchschnittsnerd.");
 $item->setImage("hoodie.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
@@ -40,17 +53,18 @@ $item->addSizePrice(new SizePrice("S", 11.90));
 $items[] = $item;
 
 $item = new Item();
-$item->setName("Pfannenwender");
-$item->setDescription(	"Diverse Pfannenwender");
+$item->setName("Laser-Foo");
+$item->setDescription(	"Diverse Gegenstände, die wir erfolgreich einer Laserbehandlung unterzogen haben.");
 $item->setImage("pfannenwender.jpg");
-$item->addSizePrice(new SizePrice("Pfannenwender mit Logo", 15.90));
-$item->addSizePrice(new SizePrice("Minecraft-Spaten", 14.90));
+$item->addSizePrice(new SizePrice("Pfannenwender mit Logo", 5.90));
+$item->addSizePrice(new SizePrice("Minecraft-Spaten", 4.90));
 
 $items[] = $item;
 
 $item = new Item();
 $item->setName("Schürze");
-$item->setDescription(	"Hoodie Hoodie Hoodie.");
+$item->setDescription(	"Es soll auch Nerds geben, die sich in die Küche trauen. Das wollen wir natürlich " .
+                        "unterstützen!");
 $item->setImage("schuerze.jpg");
 $item->addSizePrice(new SizePrice("XXL", 15.90));
 $item->addSizePrice(new SizePrice("XL", 14.90));
@@ -59,19 +73,6 @@ $item->addSizePrice(new SizePrice("M", 12.90));
 $item->addSizePrice(new SizePrice("S", 11.90));
 
 $items[] = $item;
-
-$item = new Item();
-$item->setName("T-Shirt");
-$item->setDescription(	"Hoodie Hoodie Hoodie.");
-$item->setImage("tshirt.jpg");
-$item->addSizePrice(new SizePrice("XXL", 15.90));
-$item->addSizePrice(new SizePrice("XL", 14.90));
-$item->addSizePrice(new SizePrice("L", 13.90));
-$item->addSizePrice(new SizePrice("M", 12.90));
-$item->addSizePrice(new SizePrice("S", 11.90));
-
-$items[] = $item;
-
 
 
 $targetMails = array("vorstand@raumzeitlabor.de", "mail@oliverknapp.de");
