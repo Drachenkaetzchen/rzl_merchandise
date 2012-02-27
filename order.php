f52d860f76447ef3071254bc88e4cef971a83dab
Felicitus <felicitus@felicitus.org>
2012-02-27 17:50:19 +0100

    Added order sending via mail and result page

--- order.php
+++ order.php
@@ -1,5 +1,30 @@
 <?php
+require_once 'Twig/Autoloader.php';
+Twig_Autoloader::register();
 
-echo "<pre>";
-print_r($_REQUEST);
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
 
+/**
+ * Contains the list of items to order. All orders are contained within the "item" key, and each subentry
+ * contains the item and it's size as well as the order amount.
+ */
+$orderItems = array();
+
+foreach ($_REQUEST["item"] as $key => $value) {
+	if ($value !== "") {
+		$orderItems[] = array("name" => str_pad($key, 30), "amount" => str_pad($value,4));
+	}
+}
+
+$variables = array();
+$variables["name"] = $_REQUEST["name"];
+$variables["email"] = $_REQUEST["email"];
+$variables["comments"] = $_REQUEST["comments"];
+$variables["items"] = $orderItems;
+
+$twig->display('ordercomplete.html', $variables);
+
+$mailtext = $twig->render('mailtemplate.txt', $variables);
+
+mail("felicitus@felicitus.org", "RZL-Merchandise-Shop Bestellung", $mailtext);
